da08658435d1fdd7b4bd0aa8fde3e9e2fa7dfe36
babelz <niko.halikka@gmail.com>
2016-11-10 18:01:30 +0200

    starting to work with client<->server

--- TBOServer/Authenticator.cs
+++ TBOServer/Authenticator.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using TBOLib;
 using TBOLib.Packets;
 
 namespace TBOServer
 {
     public sealed class Authenticator
     {
+        #region State helper struct
+        private struct AuthenticationState
+        {
+            public TcpClient    client;
+            public byte[]       buffer;
+            public string       auth;
+        }
+        #endregion
+
         #region Events
         public event AuthenticatorEventHandler AuthenticationFailed;
         public event AuthenticatorEventHandler AuthenticationSuccess;
@@ -21,18 +31,37 @@ namespace TBOServer
         
         private void BeginSendCallback(IAsyncResult results)
         {
+            var state = (AuthenticationState)results.AsyncState;
+
+            state.client.Client.BeginReceive(state.buffer, 0, state.buffer.Length, SocketFlags.None, BeginReceiveCallback, state);
+        }
+        private void BeginReceiveCallback(IAsyncResult results)
+        {
+            var state     = (AuthenticationState)results.AsyncState;
+            var packet    = PacketSerializer.Deserialize(state.buffer)[0];
+            var contents  = ((AuthenticationPacket)packet).contents;
+
+            if (contents == state.auth) AuthenticationSuccess?.Invoke(state.client);
+            else                        AuthenticationFailed?.Invoke(state.client);
         }
 
         public void Authenticate(TcpClient client)
         {
-            //    var data    = string.Format("VERSION: {0} | TIME: {1}", Configuration.Version, DateTime.Now);
+            var packet = new AuthenticationPacket(string.Format("VERSION: {0} | TIME: {1}", Configuration.Version, DateTime.Now));
 
-            //    var packet  = new AuthenticationPacket(data);
+            var buffer = PacketSerializer.Serialize(packet);
 
-            //    var 
-
-            //    client.Client.BeginSend(buffer, 0, buffer.Lenght, SocketFlags.None, BeginSendCallback, client);
-            //
+            client.Client.BeginSend(buffer, 
+                                    0, 
+                                    buffer.Length, 
+                                    SocketFlags.None, 
+                                    BeginSendCallback, 
+                                    new AuthenticationState()
+                                    {
+                                        client  = client,
+                                        buffer  = new byte[4096],
+                                        auth    = packet.contents
+                                    });
         }
 
         public delegate void AuthenticatorEventHandler(TcpClient client);
